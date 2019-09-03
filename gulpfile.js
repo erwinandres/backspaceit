a749c45a3092ce862679080d0e5547501dac7bd5
Erwin Larios <erwinlarios65@gmail.com>
2019-09-02 23:09:48 -0500

    Init.

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+const gulp = require('gulp');
+
+const del = require('del');
+const minifyHTML = require('gulp-minify-html');
+const minifyJS = require('gulp-terser');
+const concat = require('gulp-concat');
+const replaceHTML = require('gulp-html-replace');
+const imagemin = require('gulp-imagemin');
+const zip = require('gulp-zip');
+const checkFileSize = require('gulp-check-filesize');
+const connect = require('gulp-connect');
+
+const paths = {
+  src: {
+    html: 'src/**.html',
+    js: 'src/js/**.js',
+    images: 'src/img/**'
+  },
+  dist: {
+    dir: 'dist',
+    js: 'script.min.js',
+    images: 'dist/img'
+  }
+};
+
+gulp.task('buildHTML', () => {
+  return gulp.src(paths.src.html)
+    .pipe(replaceHTML({
+      js: paths.dist.js
+    }))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(paths.dist.dir));
+});
+
+gulp.task('buildJS', () => {
+  return gulp.src(paths.src.js)
+    .pipe(concat(paths.dist.js))
+    .pipe(minifyJS())
+    .pipe(gulp.dest(paths.dist.dir));
+});
+
+gulp.task('optimizeImages', () => {
+  return gulp.src(paths.src.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.dist.images));
+});
+
+gulp.task('zip', () => {
+  const thirteenKb = 13 * 1024;
+
+  gulp.src('zip/*')
+    .pipe(deleteFiles());
+
+  return gulp.src(`${paths.dist.dir}/**`)
+    .pipe(zip('dont-look-back.zip'))
+    .pipe(gulp.dest('zip'))
+    .pipe(checkFileSize({ fileSizeLimit: thirteenKb }));
+});
+
+gulp.task('clean', () => del('dist/**/*'));
+
+gulp.task('connect', () =>
+  connect.server({
+    port: 1337,
+    root: paths.dist.dir,
+    liveReload: true
+  })
+);
+
+gulp.task('build', gulp.series(
+  'clean',
+  gulp.parallel('buildHTML', 'buildJS', 'optimizeImages'),
+  'zip'
+));
+
+gulp.task('watch', () => {
+  gulp.watch(paths.src.html, gulp.series('buildHTML', 'zip'));
+  gulp.watch(paths.src.js, gulp.series('buildJS', 'zip'));
+  gulp.watch(paths.src.images, gulp.series('optimizeImages', 'zip'));
+});
+
+gulp.task('default', gulp.series(
+  'build',
+  gulp.parallel('connect', 'watch')
+));
